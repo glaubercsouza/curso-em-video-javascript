65d6f2d521fcdd3ccabab778b9eb425cf665e7f9
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-01-03 15:07:49 -0300

    Create ambiente.js

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,22 @@
+c = 1
+do {
+  console.log(`Passo ${c}`)
+  c++
+} while (c <= 6)
+
+/*
+var cont = 0 
+while (cont < 6) {
+  console.log(`Passo ${cont}`)
+  cont++
+}
+/*
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
