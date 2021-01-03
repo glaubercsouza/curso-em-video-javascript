185a5982b8a1afb597b041a6fd4620be43f11802
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-01-03 15:41:20 -0300

    aula14

---
+++ aulas/aula14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while (c <= 10){
+  console.log(`Passo ${c}`)
+  c++
+}
+*/
+//-----------------
+
+console.log('Vai começar...')
+for (var c = 1; c <= 4; c++){
+  console.log(`Passo ${c}`)
+}
+console.log('FIM!')
