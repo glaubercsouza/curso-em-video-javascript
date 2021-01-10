31cc7d40c1d76ccf156f2a445bec22f56f0c31e2
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-01-10 20:08:12 -0300

    aula16

--- aulas/aula16ex/ex018/script.js
+++ aulas/aula16ex/ex018/script.js
@@ -1,21 +1,12 @@
-function adicionar() {
-  var numeroadd = document.getElementById('txtadd')
-  var numeros = document.getElementById('txtnum')
-  num = Number(numeroadd.value)
-  if (num < 1 || num > 100) {
-    alert('Valor inválido ou já encontrado na lista')
-  } else {
-    let n = []
+
+
+
+
+
+/*let n = []
     n.push(num)
     var add = document.createElement('option')
     add.text = `Valor ${n} adicionado`
     add.value = `numeros${n}`
     numeros.appendChild(add)
-  }
-}
-
-function finalizar() {
-  if (n.value.length == '0') {
-    alert('Adicione valores antes de finalizar')
-  }
-}
+    */
