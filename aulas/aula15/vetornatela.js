3568adcfdd253ba7e4855b79781e3e3c24f4d28d
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-01-10 15:51:47 -0300

    ex018

--- aulas/aula15/vetornatela.js
+++ aulas/aula15/vetornatela.js
@@ -14,8 +14,8 @@ console.log(valores[5])
 for(pos = 0; pos < valores.length; pos++) {
   console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
-*/
+
 
 for(let pos in valores) {
   console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
+}*/
